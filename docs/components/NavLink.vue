80a93aac4fce9a4bcb3ca71dfcd0727939ac9bdc
Tricker <873132083@qq.com>
2023-04-11 21:13:37 +0800

    style 💄: 首页样式改造

---
+++ docs/components/NavLink.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="link-box">
+    <a
+      class="link-item"
+      v-for="item in props.links"
+      :key="item.title"
+      :target="item.target"
+      :href="withBase(item.url)"
+    >
+      <img :src="item.media" :alt="item.title" class="media" />
+      <div class="title">{{ item.title }} </div>
+    </a>
+  </div>
+</template>
+<script setup lang="ts">
+  import { withBase } from 'vitepress';
+  interface NavLink {
+    media: string;
+    title: string;
+    url: string;
+    target: '_blank' | '_self';
+  }
+  const props = defineProps({
+    links: Array<NavLink>,
+  });
+
+  const _bindGoPage = (record: NavLink) => {
+    if (record.target == '_blank') {
+      window.open(record.url);
+    } else {
+      window.location.href = record.url;
+    }
+  };
+</script>
+<style lang="scss" scoped>
+  .link-box {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 16px;
+    margin: 32px 0;
+    .link-item {
+      display: flex;
+      align-items: center;
+      padding: 16px;
+      cursor: pointer;
+      color: var(--vp-c-text-1);
+      text-decoration: none;
+      transition: transform 0.5s;
+      border-radius: 8px;
+      background: var(--vp-c-bg-alt);
+      box-shadow: var(--vp-shadow-2);
+      .media {
+        flex-shrink: 0;
+        margin-right: 16px;
+        width: 48px;
+        height: 48px;
+        border-radius: 100%;
+        box-shadow: var(--vp-shadow-2);
+      }
+      &:hover {
+        transform: translateY(-8px);
+        .media {
+          animation: spin 2s 0.2s infinite linear;
+        }
+      }
+    }
+  }
+  @media (min-width: 960px) and (max-width: 1279px) {
+    .link-box {
+      grid-template-columns: repeat(3, 1fr);
+    }
+  }
+  @media (min-width: 960px) and (max-width: 1080px) {
+    .link-box {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+  @media (min-width: 768px) and (max-width: 959px) {
+    .link-box {
+      grid-template-columns: repeat(3, 1fr);
+    }
+  }
+  @media (max-width: 768px) {
+    .link-box {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+  @media (max-width: 640px) {
+    .link-box {
+      grid-template-columns: repeat(1, 1fr);
+    }
+  }
+  @keyframes spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+</style>
--- docs/home/introduction.md
+++ docs/home/introduction.md
@@ -3,14 +3,36 @@ aside: false
 lastUpdated: false
 ---
 
+<script setup>
+import NavLink from '../components/NavLink.vue';
+const links=[{
+    media: 'https://s1.ax1x.com/2023/04/11/ppOYs9x.jpg',
+    title: '代码片段',
+    url: '/codes/intro',
+    target: '_self'
+},{
+    media: 'https://s1.ax1x.com/2023/04/11/ppOY1Nn.png',
+    title: 'Typescript 学习笔记',
+    url: '/typescript/intro', 
+    target: '_self'
+},{
+    media: 'https://s1.ax1x.com/2023/04/11/ppOtSCq.png',
+    title: '计算机网络',
+    url: '/network/http', 
+    target: '_self'
+},{
+    media: 'https://s1.ax1x.com/2023/04/11/ppOtCvT.png',
+    title: '前端面试题',
+    url: '/interview/vue', 
+    target: '_self'
+},{
+    media: 'https://s1.ax1x.com/2023/04/11/ppOt1qe.jpg',
+    title: 'Markdown 语法',
+    url: 'https://markdown.com.cn', 
+    target: '_blank'
+}]
+</script>
+
 # 简介
 
-[:link: 代码片段](/codes/intro)
-
-[:link: Typescript 学习笔记](/typescript/intro)
-
-[:link: 计算机网络](/network/http)
-
-[:link: 前端面试题](/interview/vue)
-
-[:link: Markdown 语法](https://markdown.com.cn/)
+<nav-link :links="links"/>
