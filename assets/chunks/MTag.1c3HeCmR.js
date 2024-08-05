6e1a80b5d034a459c3271c0c1c6d23a9ef321143
Tricker39 <Tricker39@users.noreply.github.com>
2024-08-05 01:59:12 +0000

    Auto deploy from Github Actions

---
+++ assets/chunks/MTag.1c3HeCmR.js
@@ -0,0 +1,1 @@
+import{d as t,c as o,r as n,n as r,o as c,_ as p}from"./framework.BVPdqVqB.js";const l=t({__name:"MTag",props:{type:{default:"default"}},setup(e){const a=e;return(s,_)=>(c(),o("span",{class:r(["tag",a.type])},[n(s.$slots,"default",{},void 0,!0)],2))}}),f=p(l,[["__scopeId","data-v-aca798e8"]]);export{f as M};
